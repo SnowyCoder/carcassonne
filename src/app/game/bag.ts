b97171d3d3abf9d0ee72adfc687496240828f9ff
Rossi Lorenzo <snowycoder@gmail.com>
2021-01-22 17:41:01 +0100

    Remove debug cards

--- src/app/game/bag.ts
+++ src/app/game/bag.ts
@@ -79,7 +79,7 @@ export class Bag {
     }
 
     static duplicateCards(original: Array<Card>): Array<Card> {
-        return original;
+        //return original;
         let res = new Array<Card>();
         for (let card of original) {
             res.push(...Array(card.quantity).fill(card))
